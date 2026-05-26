cd3e869a474039b4780ef3f7368f8f3ea22e286e
Marcos <marcosesro2812@gmail.com>
2026-05-26 08:52:24 -0500

    Estructura con funciones por referencia y valor

--- Program.cs
+++ Program.cs
@@ -2,6 +2,18 @@
 
 class Program
 {
+    // Función por referencia
+    static void CalcularDatos(int[] notas, ref double promedio, ref int maxima, ref int minima)
+    {
+        
+    }
+
+    // Función por valor
+    static int CalcularAprobados(int[] notas)
+    {
+        return 0;
+    }
+
     static void Main()
     {
 
